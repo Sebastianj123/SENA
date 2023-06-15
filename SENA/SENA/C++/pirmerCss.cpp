05d3d5fe80197ebf4fca0736c146468491133d59
Sebastianj123 <113195211+Sebastianj123@users.noreply.github.com>
2023-06-14 22:21:15 -0500

    Borrar carpeta duplicada
    Se borro la carpeta SENA de C:/SENA ya que se había duplicado

--- SENA/SENA/C++/pirmerCss.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int numero = 15;
-	cout<<numero;
-	return 0;
-}
--- SENA/SENA/Guias/Guia 6/4. Vendedor/js/script.js
+++
@@ -1,3 +0,0 @@
-var sueldo = Number(prompt('Ingrese su sueldo mesual')),
-comiciones = Number(prompt('Ingrese el numero de comiciones'));
-alert('Resivira $' + Number(sueldo + (sueldo * comiciones)) + " este mes.");
